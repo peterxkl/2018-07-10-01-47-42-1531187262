e89d4f80dc3ecd38a0f57813d339f3925cc10478
DILLON XIE (ITA-ISDC-ISD-OOCLL/ZHA) <XIEDI@OOCL.COM>
2019-07-11 16:26:30 +0800

    feat:finish t1-t3

--- src/main/java/com/thoughtworks/collection/Add.java
+++ src/main/java/com/thoughtworks/collection/Add.java
@@ -3,18 +3,34 @@ package com.thoughtworks.collection;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class Add {
     public int getSumOfEvens(int leftBorder, int rightBorder) {
-        throw new NotImplementedException();
+        int max=Math.max(leftBorder,rightBorder);
+        int min=Math.min(leftBorder,rightBorder);
+        int sum=IntStream.rangeClosed(min,max).filter((num)->{
+            return num%2==0;
+        }).sum();
+        return sum;
+
     }
 
     public int getSumOfOdds(int leftBorder, int rightBorder) {
-        throw new NotImplementedException();
+        int max=Math.max(leftBorder,rightBorder);
+        int min=Math.min(leftBorder,rightBorder);
+        int sum=IntStream.rangeClosed(min,max).filter((num)->{
+            return num%2!=0;
+        }).sum();
+        return sum;
     }
 
     public int getSumTripleAndAddTwo(List<Integer> arrayList) {
-        throw new NotImplementedException();
+        int sum=0;
+        for(int i=0;i<arrayList.size();i++){
+            sum=sum+(arrayList.get(i)*3+2);
+        }
+        return sum;
     }
 
     public List<Integer> getTripleOfOddAndAddTwo(List<Integer> arrayList) {
